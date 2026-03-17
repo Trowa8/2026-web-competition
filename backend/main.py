b8c8a568e62dc21a1c51a2b5ab5ae3824d51198b
Trowa8 <likepcandtv@gmail.com>
2026-03-17 23:07:11 +0200

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/server/healthcheck")
+def get_healthcheck():
+    return {"message": "all good"}
+
