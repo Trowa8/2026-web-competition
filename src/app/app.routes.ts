e40461e53da967fd8758853bc97bd9f9867dde4c
katrinkaia <katya2009.savchenko@gmail.com>
2026-03-24 19:21:25 +0200

    feat: initial Angular setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { TournamentList } from './pages/tournament-list/tournament-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'tournaments', component: TournamentList },
+];
