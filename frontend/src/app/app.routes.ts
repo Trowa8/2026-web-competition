0aa359e0b1757bcf963a86ac80b0e143908f7d7d
Timsyvash <timofey.syvash@gmail.com>
2026-04-05 13:26:48 +0300

    Delete frontend directory

--- frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { TournamentList } from './pages/tournament-list/tournament-list';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: Login },
-  { path: 'tournaments', component: TournamentList },
-];
