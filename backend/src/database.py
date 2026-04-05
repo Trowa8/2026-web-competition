40d79b513d26e25f9b7f11c6f746746863fe2e3c
Trowa8 <likepcandtv@gmail.com>
2026-04-05 13:06:04 +0300

    Clean up database.py

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from src.config import settings  # your settings object
+from src.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
