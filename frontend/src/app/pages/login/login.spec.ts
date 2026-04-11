7cf2102cb4e83a6ec213e86ae0dafa27daeb20cc
Timsyvash <timofey.syvash@gmail.com>
2026-04-11 15:14:22 +0300

    Delete frontend directory

--- frontend/src/app/pages/login/login.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LoginComponent } from './login';
-
-describe('LoginComponent', () => {
-  it('should create', () => {
-    expect(new LoginComponent(null as any)).toBeTruthy();
-  });
-});
--- frontend/src/app/pages/teams/team-list.component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TeamListComponent } from './team-list.component';
-
-describe('TeamListComponent', () => {
-    it('should create', () => {
-        expect(new TeamListComponent()).toBeTruthy();
-    });
-});
