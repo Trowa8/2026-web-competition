2e481b25ce726465f45ae5bd9907d4e2a0c398e7
Trowa8 <likepcandtv@gmail.com>
2026-05-01 09:35:38 +0300

    Added Team Endpoints and src

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from src.routers.auth import router as auth_router
+from src.routers.user import router as user_router
+from src.routers.tournament import router as tournament_router
 
 app = FastAPI()
 app.include_router(auth_router)
+app.include_router(user_router)
+app.include_router(tournament_router)
 
 @app.get("/server/healthcheck")
 def get_healthcheck():
