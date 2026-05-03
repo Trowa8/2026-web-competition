ad2c5dc3a7e852dbef9145f710807a18b2002c3d
IlliaSatyrenko <denzp05@gmail.com>
2026-05-04 01:17:51 +0300

    fix: fix git history

--- frontend/src/app/shared/types/team.types.ts
+++
@@ -1,8 +0,0 @@
-export type Team = {
-    teamId: number;
-    name: string;
-    description: string;
-    ownerId: number;
-    members: { userId: number; role: string; joinedAt: string }[];
-    createdAt: string;
-};
