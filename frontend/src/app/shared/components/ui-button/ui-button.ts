f2722a9d06998d2a5ccbc33a72f0bb2293078fac
katrinkaia <katya2009.savchenko@gmail.com>
2026-05-04 20:49:24 +0300

    changes

--- frontend/src/app/shared/components/ui-button/ui-button.ts
+++ frontend/src/app/shared/components/ui-button/ui-button.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 export type ButtonColor = 'primary' | 'outline' | 'leave' | 'secondary' ;
 export type ButtonType = 'button' | 'submit' | 'reset';
 export type ButtonAlignment = 'left' | 'center' | 'right';
-export type ButtonVariant = 'solid' | 'outline' | 'ghost';
 @Component({
   selector: 'ui-button',
   imports: [CommonModule],
@@ -14,11 +13,10 @@ export type ButtonVariant = 'solid' | 'outline' | 'ghost';
 export class UiButton {
   public readonly text: InputSignal<string>=input('Button');
   public readonly color: InputSignal<ButtonColor>=input<ButtonColor>('primary');
-  public readonly variant: InputSignal<ButtonVariant> = input<ButtonVariant>('solid');
   public readonly disabled: InputSignal<boolean>=input(false);
-  public readonly IsFullWidth: InputSignal<boolean> = input(false);
+  public readonly isFullWidth: InputSignal<boolean> = input(false);
   public readonly icon: InputSignal<string > = input("");
-  public readonly IconPosition: InputSignal<'left' | 'right'> = input<'left' | 'right'>('left');
+  public readonly iconPosition: InputSignal<'left' | 'right'> = input<'left' | 'right'>('left');
   public readonly type: InputSignal<ButtonType> = input<ButtonType>('button');
   public readonly alignment: InputSignal<ButtonAlignment> = input<ButtonAlignment>('center');
 
