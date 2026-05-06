a5a4fa3ea9ded5cf9dd957e12cb8c56b14357b2f
Trowa8 <likepcandtv@gmail.com>
2026-05-06 22:17:59 +0300

    Initial edits

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,7 @@ from src.routers.tournament import router as tournament_router
 from src.routers.task import router as task_router
 from src.routers.user import router as user_router
 from src.routers.team import router as team_router
+from src.routers.solution import router as solution_router
 
 app = FastAPI()
 app.include_router(auth_router)
@@ -11,6 +12,7 @@ app.include_router(tournament_router)
 app.include_router(task_router)
 app.include_router(user_router)
 app.include_router(team_router)
+app.include_router(solution_router)
 
 @app.get("/server/healthcheck")
 def get_healthcheck():
