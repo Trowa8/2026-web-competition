73d6599ee8314cf3a912fe4adc85c06176da4028
katrinkaia <katya2009.savchenko@gmail.com>
2026-05-07 16:29:45 +0300

    fixing

--- frontend/src/app/pages/create-task/create-task.ts
+++ frontend/src/app/pages/create-task/create-task.ts
@@ -1,35 +1,42 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { Router } from "@angular/router";
+//import { TaskService } from "../../shared/services/task.service";
 
 @Component({
     selector: "app-create-task",
     standalone: true,
     imports: [FormsModule, CommonModule],
     templateUrl: "./create-task.html",
-    styleUrl: "./create-task.scss",
+    styleUrl: "./create-task.css",
 })
 export class CreateTask {
+    private readonly router = inject(Router);
+    //private readonly taskService = inject(TaskService);
+
     task = {
-        title: "",
+        name: "",
         description: "",
         deadline: "",
-        tournamentId: 0,
+        tournamentId: "",
     };
 
-    constructor(private router: Router) {}
-
-    async onSubmit() {
-        if (!this.task.title || !this.task.description || !this.task.deadline || !this.task.tournamentId) {
-            alert("Будь ласка, заповніть всі обов’язкові поля");
+    public async onSubmit(): Promise<void> {
+        if (!this.task.name || !this.task.description || !this.task.deadline || !this.task.tournamentId) {
+            alert("Будь ласка, заповніть всі обов'язкові поля");
             return;
         }
 
         try {
+            // await this.taskService.createTask(this.task.tournamentId, {
+            //     name: this.task.name,
+            //     description: this.task.description,
+            //     deadline: this.task.deadline,
+            // });
             alert("Завдання успішно створено!");
             this.router.navigate(["/tournaments"]);
-        } catch (err) {
+        } catch {
             alert("Помилка при створенні завдання");
         }
     }
