9956ad8a6416a1650129a6f1292957a71cf6db7c
katrinkaia <katya2009.savchenko@gmail.com>
2026-05-14 23:04:31 +0300

    feat/tournament-detail

--- frontend/src/app/pages/tournament-detail/tournament-detail.ts
+++ frontend/src/app/pages/tournament-detail/tournament-detail.ts
@@ -1,28 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-tournament-detail',
+  selector: 'app-tournament-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './tournament-detail.html',
   styleUrls: ['./tournament-detail.css']
 })
-export class TournamentDetailComponent {
-  tournament = signal({
-    tournament_id: 1,
-    name: 'Kharkiv Open 2025',
-    description: 'Your challenge for Kharkiv Open 2025. Show your best design skills!',
-    start_date: '03.04.2025',
-    registration_deadline: '01.04.2025',
-    created_by: 10,
-    created_at: '2025-01-01'
-  });
+export class TournamentDetailsComponent implements OnInit {
+  activeTab: string = 'overview';
+  tournamentId: string | null = null;
 
-  number_of_teams = signal(16);
-  end_date = signal('10.04.2025');
+  constructor(private route: ActivatedRoute) { }
 
-  register() {
-    console.log('Реєстрація на турнір активована');
+  ngOnInit() {
+    this.tournamentId = this.route.snapshot.paramMap.get('id');
+  }
+
+  setTab(tab: string) {
+    this.activeTab = tab;
   }
 }
