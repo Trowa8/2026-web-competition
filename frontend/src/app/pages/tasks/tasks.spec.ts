5b4656ee3237c5dde6362211c9b94cea0f33c3e6
IlliaSatyrenko <denzp05@gmail.com>
2026-05-15 19:44:18 +0300

    Translate tournament titles and descriptions to Ukrainian; update CSS color codes for consistency

--- frontend/src/app/pages/tasks/tasks.spec.ts
+++ frontend/src/app/pages/tasks/tasks.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { TasksComponent } from './tasks';
+import { TasksComponent } from "./tasks";
 
-describe('Tasks', () => {
-  let component: TasksComponent;
-  let fixture: ComponentFixture<TasksComponent>;
+describe("Tasks", () => {
+    let component: TasksComponent;
+    let fixture: ComponentFixture<TasksComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TasksComponent]
-    })
-      .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TasksComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(TasksComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TasksComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
